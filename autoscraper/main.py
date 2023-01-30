d6fa372651864c1b8511cd31315582c01cf81393
gonzalezgbr <gonzalezgbr@gmail.com>
2023-01-30 19:46:33 -0300

    Agregar parseo de argumentos cmdline. Agregar opción de ver sucursales disponibles

--- autoscraper/main.py
+++ autoscraper/main.py
@@ -1,8 +1,36 @@
+import argparse
+
 from scraper import Scraper
+from storescraper import StoreScraper
+
+from autoscraper import __version__
+
+
+def parse_cmd_line_arguments():
+    """Return the parsed cmd line arguments."""
+    parser = argparse.ArgumentParser(
+        prog="autoscraper",
+        description="Scraper de productos del Hipermercado Libertad.",
+        epilog="Gracias por usar autoscraper!",
+    )
+    parser.version = __version__
+
+    parser.add_argument("-v", "--version", action="version")
+    parser.add_argument("-vc", "--ver-sucursales", action="store_true",
+                        help="Ver listado de sucursales disponibles")
+
+    return parser.parse_args()
 
 
 def main():
     """Función principal, direcciona ejecución del programa."""
+
+    args = parse_cmd_line_arguments()
+    if args.ver_sucursales:
+        stores = StoreScraper().get_stores()
+        for store in stores:
+            print(store)
+        return
 
     store_name = 'Hipermercado Sgo. Del Estero'
     scraper = Scraper(store_name)
@@ -14,7 +42,7 @@ def main():
     filename = '../data/products-hiper-sde.txt'
     with open(filename, 'w', encoding='utf8') as outfile:
         scraper.set_writer(outfile)
-        for category in categories[18:]:
+        for category in categories:
             for subcategory in category.subcategories:
                 scraper.get_products(category.name, subcategory)
 
