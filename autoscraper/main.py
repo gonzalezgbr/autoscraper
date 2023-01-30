4ffb87474d7c64379abfb2e243ff452af99db85d
gonzalezgbr <gonzalezgbr@gmail.com>
2023-01-30 16:57:29 -0300

    Agregar ejecución de scraping de productos.

--- autoscraper/main.py
+++ autoscraper/main.py
@@ -7,8 +7,16 @@ def main():
     store_name = 'Hipermercado Sgo. Del Estero'
     scraper = Scraper(store_name)
     categories = scraper.get_categories()
-    for category in categories:
-        print(category)
+    for cat in categories:
+        print(cat)
+    print(f'Total categorias: {len(categories)}')
+
+    filename = '../data/products-hiper-sde.txt'
+    with open(filename, 'w', encoding='utf8') as outfile:
+        scraper.set_writer(outfile)
+        for category in categories[18:]:
+            for subcategory in category.subcategories:
+                scraper.get_products(category.name, subcategory)
 
 
 if __name__ == '__main__':
