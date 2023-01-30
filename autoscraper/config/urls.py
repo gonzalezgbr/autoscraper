05c61eaa16f1d979e650a7569229ac9033d2dc9a
gonzalezgbr <gonzalezgbr@gmail.com>
2023-01-30 16:54:59 -0300

    Agregar build de url de productos.

--- autoscraper/config/urls.py
+++ autoscraper/config/urls.py
@@ -5,10 +5,18 @@ class UrlBuilder:
         self._BASE_URL = 'https://www.hiperlibertad.com.ar/'
         self._STORES_URL = 'files/fit-institucional-sucursales.js'
         self._PRODUCT_BASE_URL = 'api/catalog_system/pub/products/search'
-        #tecnologia/tv-y-video?O=OrderByTopSaleDESC&_from=0&_to=49&ft&sc=12
-        self._CATEGORIES_URL = 'api/catalog_system/pub/facets/search?category:"all"'
+        self._CATEGORIES_URL = 'api/catalog_system/pub/facets/search?ft'
 
     def build_categories_url(self):
         """Devuelve url para scrapear categorías."""
 
         return self._BASE_URL + self._CATEGORIES_URL
+
+    def build_product_url(self, start: int, subcategory_link: str, store: int) -> str:
+        """Devuelve url para scrapear productos por subcategoría."""
+        # ej:tecnologia/tv-y-video?O=OrderByTopSaleDESC&_from=0&_to=49&ft&sc=12
+        # La api permite como max. 50 productos por request.
+        end = start + 49
+        search_url = f'?O=OrderByTopSaleDESC&_from={start}&_to={end}&ft&sc={store}'
+
+        return self._BASE_URL + self._PRODUCT_BASE_URL + subcategory_link + search_url
